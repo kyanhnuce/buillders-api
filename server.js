a387855d63e056fa62183d62c6ce5ddf9c7d33bc
kyanhnuce <kyanhnuce@gmail.com>
2023-03-05 21:20:03 +0700

    Source API Sika 2

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+// Custom route to handle GET /users/:username
+server.get('/items/:name/:name/:name/:name', (req, res) => {
+  const username = req.params.username;
+  const user = router.db
+    .get('items')
+    .find({ username })
+    .value();
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
