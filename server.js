1859929da7cdbc6b9c6f5d76c341ab4e97203a56
kyanhnuce <kyanhnuce@gmail.com>
2023-03-05 23:57:14 +0700

    Test call API SIKA 4

--- server.js
+++ server.js
@@ -5,15 +5,33 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
-// Custom route to handle GET /items/:name
-server.get('/items/:name/:name/:name/:name', (req, res) => {
+// // Custom route to handle GET /items/:name
+// server.get('/items/:name/:name/:name/:name', (req, res) => {
+//   const name = req.params.name;
+//   const items = router.db
+//     .get('items')
+//     .find({ name })
+//     .value();
+//   if (items) {
+//     res.json(user);
+//   } else {
+//     res.sendStatus(404);
+//   }
+// });
+
+// Custom route to handle GET /items/:departmentId/users/:userId
+server.get('/items/:name/admixture/:name/sika-viscocrete/:name', (req, res) => {
   const name = req.params.name;
-  const items = router.db
+  const item = router.db
     .get('items')
     .find({ name })
+    .get('admixture')
+    .find({ name })
+    .get('sika-viscocrete')
+    .find({ name })
     .value();
-  if (items) {
-    res.json(items);
+  if (item) {
+    res.json(item);
   } else {
     res.sendStatus(404);
   }
