6feda2a3b61c08b5f7019c2e02c91b7d840e45e5
kyanhnuce <kyanhnuce@gmail.com>
2023-03-07 20:11:15 +0700

    API item 1

--- server.js
+++ server.js
@@ -1,96 +1,64 @@
-const _ = require('lodash');
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
-// // Custom route to handle GET /items/:name
-// server.get('/items/:name/:name/:name/:name', (req, res) => {
-//   const name = req.params.name;
-//   const items = router.db
-//     .get('items')
-//     .find({ name })
-//     .value();
-//   if (items) {
-//     res.json(user);
-//   } else {
-//     res.sendStatus(404);
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+// server.use(jsonServer.bodyParser)
+// server.use((req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.body.createdAt = Date.now()
 //   }
-// });
+//   // Continue to JSON Server router
+//   next()
+// })
 
-// // Custom route to handle GET /items/:departmentId/users/:userId
-// server.get('/items/:name/admixture/:name/sika-viscocrete/:name', (req, res) => {
-//   const name = req.params.name;
-//   const item = router.db
-//     .get('items')
-//     .find({ name })
-//     .get('admixture')
-//     .find({ name })
-//     .get('sika-viscocrete')
-//     .find({ name })
-//     .value();
-//   if (item) {
-//     res.json(item);
-//   } else {
-//     res.sendStatus(404);
-//   }
-// });
-
-// server.get('/items/:itemName/admixtures/:admixtureName/viscocretes/:viscocreteName/datas/:dataName', (req, res) => {
-//   const itemName = req.params.itemName;
-//   const admixtureName = req.params.admixtureName;
-//   const viscocreteName = req.params.viscocreteName;
-//   const dataName = req.params.dataName;
-//   const item = _.find(router.db.get('items').value(), { name: itemName });
-//   if (item) {
-//     const admixture = _.find(item.admixtures, { name: admixtureName });
-//     if (admixture) {
-//       const viscocrete = _.find(admixture.viscocretes, { name: viscocreteName });
-//       if (viscocrete) {
-//         const data = _.find(admixture.datas, { name: dataName });
-//         if (data) {
-//           res.json(data);
-//         } else {
-//           res.sendStatus(404);
-//         }
-//       } else {
-//         res.sendStatus(404);
-//       }
-//     } else {
-//       res.sendStatus(404);
-//     }
-//   } else {
-//     res.sendStatus(404);
-//   } 
-// });
-
-server.get('/departments/:departmentName', (req, res) => {
-  const departmentName = req.params.departmentName;
-  // const projectName = req.params.projectName;
-  const department = _.find(router.db.get('departments').value(), { name: departmentName });
-  if (department) {
-    res.json(department);
-    // const project = _.find(department.projects, { name: projectName });
-
-    // if (project) {
-    //   res.json(project);
-    // } else {
-    //   res.sendStatus(404);
-    // }
-  } else {
-    res.sendStatus(404);
+// Endpoint cho việc tìm kiếm item theo query parameters
+server.get('/items/:name/search', (req, res) => {
+  const { q, type } = req.query;
+  const name = req.params.name;
+  if (q && type === 'less') {
+    const results = router.db
+      .get('items')
+      .find({ name })
+      .get('data')
+      .filter(
+        (result) =>
+          result.name.toLowerCase().includes(q.toLowerCase()) ||
+          result.description.toLowerCase().includes(q.toLowerCase()),
+      )
+      .slice(0, 5)
+      .value();
+    return res.json(results);
   }
 });
 
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+server.use(jsonServer.bodyParser);
+server.use('/items/:name/data', (req, res, next) => {
+  const name = req.params.name;
+  if (req.method === 'GET') {
+    const item = router.db
+      .get('items')
+      .find({ name })
+      .get('data')
+      .filter((item) => item.name)
+      .value();
+    return res.json(item);
+  }
+  next();
 });
 
+server.use(router);
 
-server.use(router)
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running');
+});
